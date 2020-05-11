b4e470c9fac144e96cb5152041fcf2c5ec967fde
Usagui22 <danita9516@gmail.com>
2020-05-10 23:49:02 -0400

    mejoras en Calculadora

--- Elementos_Programacion/src/elementos_programacion/Calculadora.java
+++ Elementos_Programacion/src/elementos_programacion/Calculadora.java
@@ -5,6 +5,8 @@
  */
 package elementos_programacion;
 
+import java.lang.Math;
+
 /**
  *
  * @author Usagui
@@ -12,12 +14,17 @@ package elementos_programacion;
 public class Calculadora {    
     private int valor1;
     private int valor2;
+    private int valor3;
     private int resultado=0;
+    
     
     public Calculadora(int numero1, int numero2){                      
         valor1 = numero1;
         valor2 = numero2;        
-    }    
+    } 
+    public Calculadora(int numero3){
+        valor3 = numero3;
+    }
     public void suma(){         
         resultado = valor1 + valor2;        
     }
@@ -34,23 +41,22 @@ public class Calculadora {
         System.out.println("base: "+valor1+" y el exponente: "+valor2);
         resultado = (int) Math.pow(valor1,valor2);
     }
-    public void logaritmo10(int numero){
-        resultado = (int) Math.log10(numero);
+    public void logaritmo10(){
+        resultado = (int) Math.log10(valor3);
     }
-    public void logaritmoE(int numero){
-        resultado =(int) Math.log(numero);
+    public void logaritmoE(){
+        resultado =(int) Math.log(valor3);
     }
-    public void coseno(int angulo){
-        resultado = (int)Math.cos(angulo);
+    public void coseno(){
+        resultado = (int)Math.cos(valor3);
     }
-    public void seno(int angulo){
-        resultado = (int)Math.sin(angulo);
+    public void seno(){
+        resultado = (int)Math.sin(valor3);
     }
-    public void tangente(int angulo){
-        resultado = (int)Math.tan(angulo);
+    public void tangente(){
+        resultado = (int)Math.tan(valor3);
     }
     public void mostrarResultado(){
         System.out.println("El resultado es:" + resultado);
-    }
-    
+    }      
 }
--- Elementos_Programacion/src/elementos_programacion/Elementos_Programacion.java
+++ Elementos_Programacion/src/elementos_programacion/Elementos_Programacion.java
@@ -5,6 +5,8 @@
  */
 package elementos_programacion;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Usagui
@@ -14,28 +16,120 @@ public class Elementos_Programacion {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        Calculadora prueba = new Calculadora(3,5);
-        prueba.suma();
-        prueba.mostrarResultado();
-        prueba.resta();
-        prueba.mostrarResultado();
-        prueba.multiplicacion();
-        prueba.mostrarResultado();
-        prueba.division();
-        prueba.mostrarResultado();
-        prueba.coseno(33);
-        prueba.mostrarResultado();
-        prueba.seno(66);
-        prueba.mostrarResultado();
-        prueba.tangente(98);
-        prueba.mostrarResultado();
-        prueba.logaritmo10(15);
-        prueba.mostrarResultado();
-        prueba.logaritmoE(36);
-        prueba.mostrarResultado();
-        prueba.potencia();        
-        prueba.mostrarResultado();        
+    public static void main(String[] args) {        
+        Scanner entradaScanner = new Scanner(System.in);
+        String bandera;
+        System.out.println("Iniciando...");        
+        System.out.println("Presione start para empezar");
+        bandera = entradaScanner.next();        
+        if(bandera.equals("start")==true){
+        mostrarOperaciones();          
+        System.out.println("Ingrese que operacion va a realizar:");  
+        int opcion = entradaScanner.nextInt();
+        seleccionarOperacion(opcion,entradaScanner);                                                                
+        }else{
+        System.out.println("Cerrando...");
+        }
     }
-    
+    public static void mostrarOperaciones(){
+        System.out.println("Tenemos las siguientes operaciones:");
+        System.out.println("1. Suma");
+        System.out.println("2. Resta");
+        System.out.println("3. Multiplicación");
+        System.out.println("4. División");
+        System.out.println("5. Potencia");
+        System.out.println("6. Logaritmo de Base 10");
+        System.out.println("7. Logaritmo de Base e");
+        System.out.println("8. coseno de un angulo");
+        System.out.println("9. seno de un angulo");
+        System.out.println("10. tangente de un angulo");        
+    }
+    public static void seleccionarOperacion(int operacion, Scanner nuevaEntrada){         
+        Calculadora prueba;        
+        int primerValor;
+        int segundoValor;
+        int entrada3;                
+        switch(operacion){
+                case 1:                                
+                    System.out.println("Ingrese Primer Valor a Sumar");
+                    primerValor = nuevaEntrada.nextInt();
+                    System.out.println("Ingrese Segundo Valor a Sumar");
+                    segundoValor = nuevaEntrada.nextInt();
+                    prueba = new Calculadora(primerValor,segundoValor);
+                    prueba.suma();
+                    prueba.mostrarResultado();                    
+                  break;
+                case 2:           
+                    System.out.println("Ingrese Primer Valor a Restar");
+                    primerValor = nuevaEntrada.nextInt();
+                    System.out.println("Ingrese Segundo Valor a Restar");
+                    segundoValor = nuevaEntrada.nextInt();
+                    prueba = new Calculadora(primerValor,segundoValor);
+                    prueba.resta();
+                    prueba.mostrarResultado();
+                  break;   
+                case 3:           
+                    System.out.println("Ingrese Primer Valor a multiplicar");
+                    primerValor = nuevaEntrada.nextInt();
+                    System.out.println("Ingrese Segundo Valor a multiplicar");
+                    segundoValor = nuevaEntrada.nextInt();
+                    prueba = new Calculadora(primerValor,segundoValor);
+                    prueba.multiplicacion();
+                    prueba.mostrarResultado();
+                  break;     
+                case 4:
+                    System.out.println("Ingrese Primer Valor a dividir");
+                    primerValor = nuevaEntrada.nextInt();
+                    System.out.println("Ingrese Segundo Valor a dividir");
+                    segundoValor = nuevaEntrada.nextInt();
+                    prueba = new Calculadora(primerValor,segundoValor);
+                    prueba.division();
+                    prueba.mostrarResultado();
+                  break;
+                case 5:
+                    System.out.println("Ingrese un numero: ");
+                    primerValor = nuevaEntrada.nextInt();
+                    System.out.println("Elevado a...");
+                    segundoValor = nuevaEntrada.nextInt();
+                    prueba = new Calculadora(primerValor,segundoValor);
+                    prueba.potencia();
+                    prueba.mostrarResultado();
+                  break;
+                case 6:                    
+                    System.out.println("Ingrese un numero a calcular");
+                    entrada3 = nuevaEntrada.nextInt();
+                    prueba = new Calculadora(entrada3);
+                    prueba.logaritmo10();     
+                    prueba.mostrarResultado();
+                  break;
+                case 7:
+                    System.out.println("Ingrese un numero a calcular");
+                    entrada3 = nuevaEntrada.nextInt();
+                    prueba = new Calculadora(entrada3);
+                    prueba.logaritmoE();      
+                    prueba.mostrarResultado();
+                  break;
+                case 8:
+                    System.out.println("Ingrese un angulo a calcular");
+                    entrada3 = nuevaEntrada.nextInt();
+                    prueba = new Calculadora(entrada3);
+                    prueba.coseno();          
+                    prueba.mostrarResultado();
+                  break;
+                case 9:
+                    System.out.println("Ingrese un angulo a calcular");
+                    entrada3 = nuevaEntrada.nextInt();
+                    prueba = new Calculadora(entrada3);
+                    prueba.seno();            
+                    prueba.mostrarResultado();
+                  break;
+                case 10:
+                    System.out.println("Ingrese un angulo a calcular");
+                    entrada3 = nuevaEntrada.nextInt();
+                    prueba = new Calculadora(entrada3);
+                    prueba.tangente();        
+                    prueba.mostrarResultado();
+                  break;
+            }
+    }   
 }
